7c489f402b693e436bf791c6a556168edb7cb9ec
JosueRod17 <josuerod1723@gmail.com>
2026-06-30 23:28:17 -0300

    Ultimos ajustes.

---
+++ backend/README.md
@@ -0,0 +1,21 @@
+# Backend
+
+## Instalação
+
+```bash
+py -m pip install -r requirements.txt
+```
+
+## Executar
+
+```bash
+py -m uvicorn app.main:app --reload
+```
+
+## Documentação
+
+Após iniciar a API:
+
+```text
+http://127.0.0.1:8000/docs
+```
--- backend/app/dados.py
+++ backend/app/dados.py
@@ -3,25 +3,25 @@ from .modelos import Conta, TipoConta
 contas = [
     Conta(
         id=1,
-        titular="João",
+        titular="Josué",
         tipo=TipoConta.CORRENTE,
         saldo=1000.0
     ),
     Conta(
         id=2,
-        titular="Maria",
+        titular="Júlia",
         tipo=TipoConta.CORRENTE,
         saldo=50.0
     ),
     Conta(
         id=3,
-        titular="José",
+        titular="José Inácio",
         tipo=TipoConta.POUPANCA,
         saldo=800.0
     ),
     Conta(
         id=4,
-        titular="Ana",
+        titular="Ana Clara",
         tipo=TipoConta.POUPANCA,
         saldo=100.0
     ),
